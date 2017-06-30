c4147ed4d2028334a2ab943718183bd994c96af4
Karun Japhet <karun@japhet.in>
2017-06-30 16:44:18 -0400

    Sorting data on the info endpoint

--- src/main/java/me/karun/InfoEndpointBuilder.java
+++ src/main/java/me/karun/InfoEndpointBuilder.java
@@ -3,12 +3,14 @@ package me.karun;
 import com.google.gson.Gson;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 class InfoEndpointBuilder {
   private final String appInfo;
 
   InfoEndpointBuilder(final Config config, final Gson gson) {
-    final Map<String, String> info = config.valuesForKey("build");
+    final Map<String, String> info = new TreeMap<>();
+    info.putAll(config.valuesForKey("build"));
     info.putAll(config.valuesForKey("git"));
 
     this.appInfo = gson.toJson(info);
