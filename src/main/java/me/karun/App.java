f3a94ddbb6bf0fa4af0b8c48624c5cb258af4fea
Karun Japhet <karun@japhet.in>
2017-06-30 14:05:26 -0400

    Handle startup errors

--- src/main/java/me/karun/App.java
+++ src/main/java/me/karun/App.java
@@ -1,14 +1,22 @@
 package me.karun;
 
+import lombok.extern.slf4j.Slf4j;
+
 import static me.karun.BeanManager.beanManager;
 import static spark.Spark.*;
 
+@Slf4j
 public class App {
   public static void main(String[] args) {
     port(beanManager().config().getInt("server.port"));
 
-    final OrderController controller = beanManager().orderController();
+    initExceptionHandler((e) -> {
+      log.error("Unable to start server", e);
+      stop();
+      log.error("Stopped web server due to startup failure. Check logs for details.");
+    });
 
+    final OrderController controller = beanManager().orderController();
     post("/order", "application/json", controller::postOrder);
     get("/order/:id", "application/json", controller::getOrder);
   }
